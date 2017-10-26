5d3f248e157e8a2ecdc97ce10145bb31974d0278
stephaniejaena <sjaenasarigumba@gmail.com>
2017-10-26 14:41:42 +0800

    DONE

---
+++ index.js
@@ -0,0 +1,14 @@
+// function save() {
+// // var x = document.forms["form1"]["name"].value;
+// // var y = document.forms["form1"]["organization"].value;
+// // var a = document.forms["form1"]["phonenumber"].value;
+// // var b = document.forms["form1"]["email"].value;
+
+// // localStorage.setItem("name",x);
+// // localStorage.setItem("organization",y);
+// // localStorage.setItem("phonenumber",a);
+// // localStorage.setItem("email",b);
+// VAR SAVE = document.GetElementById('name').value;
+// console.log(save);
+// localStorage.setItem(save);
+// }
